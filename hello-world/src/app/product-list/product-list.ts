20a845163fbd260307ce4e0d9dcc116aa4b338e0
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-11-04 05:10:44 +0300

    creating product-list component

---
+++ hello-world/src/app/product-list/product-list.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  imports: [],
+  templateUrl: './product-list.html',
+  styleUrl: './product-list.css'
+})
+export class ProductList {
+  protected readonly title = signal('product list page')
+}
