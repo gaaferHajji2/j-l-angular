33030d243bbafaca4cb69c1b7d7578cdae9fd715
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-11-04 05:47:00 +0300

    Testing if && else && for

--- hello-world/src/app/product-list/product-list.ts
+++ hello-world/src/app/product-list/product-list.ts
@@ -1,4 +1,5 @@
 import { Component, signal } from '@angular/core';
+import { Product } from './product';
 
 @Component({
   selector: 'app-product-list',
@@ -9,4 +10,11 @@ import { Component, signal } from '@angular/core';
 export class ProductList {
   protected readonly title = signal('product list page')
   protected readonly dataTitle = 'test'
+  products: Product[] = [
+    {title: 'JLoka-01', id: 1},
+    {title: 'JLoka-02', id: 2},
+    {title: 'JLoka-03', id: 3},
+    {title: 'JLoka-04', id: 4},
+    {title: 'JLoka-05', id: 5},
+  ];
 }
