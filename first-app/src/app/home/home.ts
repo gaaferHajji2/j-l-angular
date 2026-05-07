f3cedbea68c047d366ecb5dd10b59f27afdd5b80
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-07 06:00:50 +0300

    Adding dummy data to home

--- first-app/src/app/home/home.ts
+++ first-app/src/app/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {HousingLocation} from '../housing-location/housing-location';
+import {HousingLocationInfo} from '../housinglocation';
+
 
 
 @Component({
@@ -8,4 +10,19 @@ import {HousingLocation} from '../housing-location/housing-location';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+  housingLocation: HousingLocationInfo = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
+
+
+}
